5902088dd084d611e11434ce200345c320c6e92d
jasir-pv <jasirahsanpv40@gmail.com>
2025-02-26 22:32:10 +0530

    fix : babel config error fixed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView, View, Text, StatusBar } from 'react-native';
 export default function HomeScreen() {
   return (
   <SafeAreaView className='flex items-center justify-center bg-red'>
-      <Text> Welcome to Spotify</Text>
+      <Text className='m-10 tex'> Welcome to Spotify</Text>
 
   </SafeAreaView>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"], 
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary: "#FF0000",
+        green: "#089637",
+      },
+    },
   },
   plugins: [],
 }
-
