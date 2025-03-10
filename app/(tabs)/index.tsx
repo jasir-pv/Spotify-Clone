832d6f585ea52a6492c9180cf7f4381a31755e48
jasir-pv <jasirahsanpv40@gmail.com>
2025-03-10 23:26:56 +0530

    global.css added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView, View, Text, StatusBar } from 'react-native';
 export default function HomeScreen() {
   return (
   <SafeAreaView className='flex items-center justify-center bg-red'>
-      <Text className='m-10 tex'> Welcome to Spotify</Text>
+      <Text className='m-10 text-xl font-bold'> Welcome to Spotify</Text>
 
   </SafeAreaView>
   );
